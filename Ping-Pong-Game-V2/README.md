683d4665328c6c4befa21672b6f5c45de338c9e6
KrishaKalola <KrishaKalola@gmail.com>
2023-03-22 22:13:23 +0530

    done

--- Ping-Pong-Game-V2/README.md
+++
@@ -1,2 +0,0 @@
-# Ping-Pong-Game
-Hope you have a great time while playing this game :)
--- j.js
+++ j.js
@@ -1,8 +1,16 @@
 import Ball from "./Ball.js"
 import Paddle from "./Paddle.js"
 
+var lastScroll = 0;
+var currentScroll = 0;
+var diff = 0;
+var nTop = 50;
+
+var scroller = document.getElementById('game');
+
 const ball = new Ball(document.getElementById("ball"))
 const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const playerPaddleJenish = document.getElementById("player-paddle");
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
 const playerScoreElem = document.getElementById("player-score")
 const computerScoreElem = document.getElementById("computer-score")
@@ -45,5 +53,22 @@ function handleLose() {
 document.addEventListener("mousemove", e => {
   playerPaddle.position = (e.y / window.innerHeight) * 100
 })
+
+scroller.scrollTop = 100
+scroller.onscroll = (e) => {
+  console.log('hello');
+  currentScroll = scroller.scrollTop;
+  diff = currentScroll - lastScroll;
+  lastScroll = currentScroll;
+  console.log(diff)
+  if(diff > 0){
+    nTop -= 1;
+  }
+  else{
+    nTop += 1;
+  }
+  playerPaddle.position = nTop;
+  //playerPaddleJenish.style.top = nTop + 'px';
+}
 
 window.requestAnimationFrame(update)
